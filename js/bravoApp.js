272b1396c1e1019daff7238013fde8c7e6b738fc
Ariel Reis <reis.ariel@outlook.com>
2017-05-24 23:13:08 -0300

    Adição dos gráficos
    Teste de gráfico

--- js/bravoApp.js
+++ js/bravoApp.js
@@ -7,3 +7,29 @@ var bravo = new Framework7({
 
 bravo.loginScreen(document.getElementById('login-screen'), false);
 
+var ctx = document.getElementById('myChart').getContext('2d');
+
+
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ["Alimentação", "Malhação", "Aeróbica"],
+        datasets: [{
+            data: [12, 19, 5],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ]
+        }]
+    },
+    options: {
+    	legend: {
+    		display: true,
+    		position: 'bottom',
+    		fullWidth: true,
+    		fontSize: 26,
+    		boxWidth: 20
+    	}
+    }
+});
